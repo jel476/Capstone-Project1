84840e063f3e3d2a90ed551664169939d35c5698
jel476 <jlondono476@outlook.com>
2019-01-03 23:53:47 -0800

    1

---
+++ MyCode.R
@@ -0,0 +1,17 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+weather<- read.delim2(file = "KNYC.txt")
+
+colnames(weather) <- c("1")
+
+weather <- separate(weather, 1, c("Site","Date","Hour","Temperature","Dewpoint","RH","WindDir","Windspeed","CldFrac","MSLP","Weather","Precip","Source"), 
+         sep = ",")
+
+weather <- select(weather, Date:Temperature, Weather:Precip)
+
+weather <- filter(weather, grepl('2014', Date))
+
+View(weather)
+
