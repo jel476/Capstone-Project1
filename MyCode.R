576a454c857bde2c9cc1cb55bbbfdf7f27f4d8cf
jel476 <jlondono476@outlook.com>
2019-01-04 23:58:23 -0800

    Uber data compiled

--- MyCode.R
+++ MyCode.R
@@ -35,7 +35,49 @@ april$Date.Time <- as.POSIXct(april$Date.Time, format = "%m/%d/%Y %H:%M:%S")
 
 april <- separate(april, Date.Time, c("Date", "Time"), sep = " ")
 
-View(april)
+
+
+may <- read.csv("uber-raw-data-may14.csv")
+
+may$Date.Time <- as.POSIXct(may$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+
+may <- separate(may, Date.Time, c("Date", "Time"), sep = " ")
+
+
+june <- read.csv("uber-raw-data-jun14.csv")
+
+june$Date.Time <- as.POSIXct(june$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+
+june <- separate(june, Date.Time, c("Date", "Time"), sep = " ")
+
+
+july <- read.csv("uber-raw-data-jul14.csv")
+
+july$Date.Time <- as.POSIXct(july$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+
+july <- separate(july, Date.Time, c("Date", "Time"), sep = " ")
+
+
+august <- read.csv("uber-raw-data-aug14.csv")
+
+august$Date.Time <- as.POSIXct(august$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+
+august <- separate(august, Date.Time, c("Date", "Time"), sep = " ")
+
+
+september <- read.csv("uber-raw-data-sep14.csv")
+
+september$Date.Time <- as.POSIXct(september$Date.Time, format = "%m/%d/%Y %H:%M:%S")
+
+september <- separate(september, Date.Time, c("Date", "Time"), sep = " ")
 
 
 
+uber <- april %>% 
+  bind_rows(may) %>% 
+  bind_rows(june) %>% 
+  bind_rows(july) %>% 
+  bind_rows(august) %>% 
+  bind_rows(september)
+
+View(uber)
